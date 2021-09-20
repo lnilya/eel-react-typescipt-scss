ad5c1038f7ec501c24246ba6d90c43cac0025607
Ilya Shabanov <ilya@one-extra.de>
2021-09-20 10:53:30 -0700

    Changed the eel expose functions, to fix errors with minification

--- public/eelJsInterface.js
+++ public/eelJsInterface.js
@@ -6,10 +6,13 @@
 //Not that this file needs to be a .js file otherwise eel won't be able to detect it.
 //Second the expose function needs to be literally "eel.expose...", since the way eel
 //works is to scan .js files for the occurance of eel . expose(...)
+//the file is located in public so it doesn't get minifier or in any way touched by buildchain.
 
 const eel = window['eel'];
 
-eel.expose(hello,'hello');
-function hello(x) {
-  console.log('Python says: ' + x);
+if(eel){
+    eel.expose(hello,'hello');
+    function hello(x) {
+        window.__el_js_hello(x)
+    }
 }
